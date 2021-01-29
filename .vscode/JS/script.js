4f8cccec612a5cb6700d9ab384d7e14a99374421
Anton <70876693+Antonpupil@users.noreply.github.com>
2021-01-29 21:52:59 +0300

    перенес папку с JS

--- .vscode/JS/script.js
+++
@@ -1,8 +0,0 @@
-<!--[if lt IE 9]>
- <script>
-  var e = ("article,aside,figcaption,figure,footer,header,hgroup,nav,section,time").split(',');
-  for (var i = 0; i < e.length; i++) {
-    document.createElement(e[i]);
-  }
- </script>
-<![endif]-->
